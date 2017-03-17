18d6d4576d2ba7dfc33151e92a643505ee5c3b34
Chase Armstrong <chasejarms@gmail.com>
2017-03-16 20:50:57 -0700

    Work with action cable

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def evaluate_time(created_at)
     time = (Time.now.yday - created_at.to_time.yday)
-    time == 0 ? "Today" : time == 1 ? "Yesterday" : "#{time} days ago"
+    time <= 0 ? "Today" : time == 1 ? "Yesterday" : "#{time} days ago"
   end
 end
