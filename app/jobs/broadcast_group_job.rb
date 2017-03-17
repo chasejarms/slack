a45ac66577c24a71d97fc512cd1c4b3c7c31098c
Chase Armstrong <chasejarms@gmail.com>
2017-03-17 09:17:28 -0700

    comment out other line to push to heroku

--- app/jobs/broadcast_group_job.rb
+++ app/jobs/broadcast_group_job.rb
@@ -1,9 +1,9 @@
 class BroadcastGroupJob < ApplicationJob
-  queue_as :default
-
-  def perform(message, channel)
-    # Do something later
-    message = Api::Message.render 'api/messages/show', locals: { message: message }
-    ActionCable.server.broadcast("group_#{params[:groupName]}", message: JSON.parse(message) }
-  end
+  # queue_as :default
+  #
+  # def perform(message, channel)
+  #   # Do something later
+  #   message = Api::Message.render 'api/messages/show', locals: { message: message }
+  #   ActionCable.server.broadcast("group_#{params[:groupName]}", message: JSON.parse(message) }
+  # end
 end
